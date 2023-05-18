346c4802f551e41a52d1e9e8e997e7e512d5ada0
Grade12student <52668227+Grade12student@users.noreply.github.com>
2023-05-18 11:46:29 -0500

    Create What's the pattern?.py

---
+++ What's the pattern?.py
@@ -0,0 +1,7 @@
+from itertools import cycle
+
+def find_pattern(sequence):
+    diffs = [y - x for x, y in zip(sequence, sequence[1:])]    
+    for i in range(1, len(diffs) + 1):
+        if len(diffs) % i == 0 and all(a == b for a, b in zip(diffs, cycle(diffs[:i]))): 
+            return diffs[:i]
