824b3decc251e22d99360b045e4bdea80502f965
Grade12student <52668227+Grade12student@users.noreply.github.com>
2023-05-25 09:21:59 -0500

    Kata python3

--- What's the pattern?.py
+++ What's the pattern?.py
@@ -5,3 +5,4 @@ def find_pattern(sequence):
     for i in range(1, len(diffs) + 1):
         if len(diffs) % i == 0 and all(a == b for a, b in zip(diffs, cycle(diffs[:i]))): 
             return diffs[:i]
+#
